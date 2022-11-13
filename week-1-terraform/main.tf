0229c89d71eef757e46bc2379f0f7440c79cff7d
vedanthv <vedanthvbaliga@gmail.com>
2022-11-13 14:09:14 +0530

    provider creds

--- week-1-terraform/main.tf
+++ week-1-terraform/main.tf
@@ -9,5 +9,36 @@ terraform {
   }
 }
 
+# adds pre defined resources and data sources that terraform can manage
+provider "google" {
+  project = var.project
+  region = var.region
+  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "${local.data_lake_bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+  location      = var.region
+
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true // derletes all objects of bucket
+}
+
 
 
